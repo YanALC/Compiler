f6ecdb7d883408e9cdf3cf148fa629a0201112d4
Yan Lobianchi <yanalc2016@outlook.com>
2019-02-26 12:17:06 -0300

    retirando comentários

--- src/test/groovy/LexerTest.groovy
+++ src/test/groovy/LexerTest.groovy
@@ -52,27 +52,6 @@ else
 		
 		then:
 		SyntaxException ex = thrown()
-		
 		assert ex.message, "Os seguintes padrões não são reconhecidos: " + [2: "\"ok\""]
-
-//		assert Lexer.tokens, [
-//				new Token(EnumTiposToken.IF, "if"),
-//				new Token(EnumTiposToken.ELSE, "else"),
-//				new Token(EnumTiposToken.ATRIB, "="),
-//				new Token(EnumTiposToken.NUM, "1.1"),
-//				new Token(EnumTiposToken.NUM, "2"),
-//				new Token(EnumTiposToken.NUM, "3"),
-//				new Token(EnumTiposToken.OPR_REL, ">"),
-//				new Token(EnumTiposToken.DELIM, "("),
-//				new Token(EnumTiposToken.DELIM, ")"),
-//				new Token(EnumTiposToken.DELIM, ";"),
-//				new Token(EnumTiposToken.ID, "x"),
-//				new Token(EnumTiposToken.ID, "variavel")
-//		]
-//
-//		assert Lexer.variaveis, [
-//				new Variavel("x"),
-//				new Variavel("variavel")
-//		]
 	}
 }
